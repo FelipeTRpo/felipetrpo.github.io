48d05d6cddd089052031dfaf433ccc290dfdede3
FelipeTRpo <felipetrpoke@hotmail.com>
2021-11-18 22:37:12 -0300

    adicionado meus hobbies

---
+++ meus-hobbies/inserir-conteudo-tag.js
@@ -0,0 +1,18 @@
+
+var idTag
+var texto
+
+function adicionartexto(idTag, texto,){
+    var element = document.getElementById(idTag);
+    element.innerHTML = texto;
+}
+adicionartexto('titulo-site', 'Meu hobbie');
+adicionartexto('nome', 'Felipe Tessarollo Ramos');
+adicionartexto('idade', '25 anos');
+adicionartexto('estado', 'Colatina-ES');
+adicionartexto('formacao', 'Formado em Eng Química');
+adicionartexto('asp-dev', 'Aspirante a dev');
+adicionartexto('titulo-meu-hobbie', 'Meu hobbie é tirar foto de paisagens!');
+adicionartexto('sobre-meu-hobbie', 'Depois que fui em um intercambio, eu comecei a notar varias paisagens e gostar do que elas tinha para me mostrar, e o desejo de capturar o momento que eu estava vendo ficou grande, comecei a tirar foto de tudo que eu achava agradavel ao meu olhar.');
+adicionartexto('link-para-meu-instagram', 'E então criei um novo instagrem pra postar as fotos de paisagem');
+adicionartexto('o-footer', '© 2021 Felipe Tessarollo Ramos');
